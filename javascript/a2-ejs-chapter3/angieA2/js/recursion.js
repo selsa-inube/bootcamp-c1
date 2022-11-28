75434c56528986f6add2a99adb8a75429c8af6ff
angie <angiepinillanova@gmail.com>
2022-11-28 15:43:21 -0500

    adjustment activity

--- javascript/a2-ejs-chapter3/angieA2/js/recursion.js
+++ javascript/a2-ejs-chapter3/angieA2/js/recursion.js
@@ -2,27 +2,22 @@ console.log("2) Exercise recursion");
 
 
 function isEven(n) {
-   if (Number.isInteger(n)) {
-      if (n >= 0) {
-         if (n == 0) {
-            return true;
-         } else if (n == 1) {
-            return false;
-         } else {
-            return isEven(n - 2);
-         }
-      } else {
-         return "?? -- Is not positive whole number";
-      }
-
-   } else {
-      return "Is not a whole number";
+   if (!(Number.isInteger(n)) || !(n >= 0)) {
+      return "?? -- Is not positive whole number";
    }
 
+   if (n == 0) {
+      return true;
+   }
+
+   if (n == 1) {
+      return false;
+   } 
+
+   return isEven(n - 2);
+ 
 }
-console.log(isEven(50));
+console.log(isEven(50));           
 console.log(isEven(75));
 console.log(isEven(-1));
 console.log(isEven(2.5));
-
-console.log("\n");
