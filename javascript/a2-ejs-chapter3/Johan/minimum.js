373467aeb99549ad918df96713c039bfe25684cb
JohanNova <johangarcianova14@gmail.com>
2022-12-08 23:25:34 -0500

    correcion*

--- javascript/a2-ejs-chapter3/Johan/minimum.js
+++ javascript/a2-ejs-chapter3/Johan/minimum.js
@@ -1,7 +1,15 @@
 //creamos la funcion "min" con los parametros de los dos valores numericos
 function min(number1, number2) {
-    //retornamos el numero que sea menor entre los parametros con la funcion estandar "math.min"
-    return Math.min(number1, number2);
+    //creamos la condicion la cual retorna el number2 en caso que el number1 sea mayor
+    if (number1 > number2) {
+        return number2
+        //verificamos que los dos numeros no sean iguales
+    } else if (number1 === number2) {
+        return "the numbers are equal"
+    } else{
+        //retornamos number1 en el caso que number2 sea mas grande
+        return number1
+    }
 }
 
 //ejecutamos las pruebas correspondientes en la consola
